51c83afe23e8a93a45a5b23351dd0f40c4f62e7e
Karol <83913433+xKarol@users.noreply.github.com>
2022-12-20 11:49:09 +0000

    Add loading env variables

--- main.go
+++ main.go
@@ -4,9 +4,16 @@ import (
 	"app/routes"
 
 	"github.com/gin-gonic/gin"
+	"github.com/joho/godotenv"
 )
 
 func main() {
+	err := godotenv.Load()
+
+	if err != nil {
+		panic(err)
+	}
+
 	r := gin.Default()
 
 	routes.Init(r)
