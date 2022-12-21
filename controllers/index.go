f29c2d5bfe190dce1dac503fd36ff56b1d108806
Karol <83913433+xKarol@users.noreply.github.com>
2022-12-21 15:02:42 +0000

    shortCode less or eq 20 characters validation

--- controllers/index.go
+++ controllers/index.go
@@ -12,7 +12,7 @@ import (
 func CreateShortLink(c *gin.Context) {
 	var body struct {
 		Url       string `json:"url" binding:"required,url"`
-		ShortCode string `json:"shortCode" binding:"required,alphanum"`
+		ShortCode string `json:"shortCode" binding:"required,alphanum,lte=20"`
 	}
 
 	if err := c.ShouldBind(&body); err != nil {
